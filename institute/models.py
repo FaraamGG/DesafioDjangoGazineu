ff33010a1048033d9f78695dbfb0386c3c1d5aeb
Bruno da Silva Gazineu <bsgazineu@hotmail.com>
2022-06-13 23:31:15 -0300

    Update models

--- institute/models.py
+++ institute/models.py
@@ -7,6 +7,9 @@ from django.utils.timezone import now
 class Teacher(models.Model):
     name = models.CharField(max_length=65)
 
+    def __str__(self):
+        return self.name
+
 
 class Course(models.Model):
     title = models.CharField(max_length=65)
@@ -16,10 +19,13 @@ class Course(models.Model):
     ends_at = models.DateTimeField(default=now)
     total_hours = models.IntegerField()
     more_info = models.TextField(default="")
-    teacher = models.CharField(max_length=65)
+    #teacher = models.CharField(max_length=65)
     cover = models.ImageField(
         upload_to='institute/covers/')
     is_published = models.BooleanField(default=False)
+    teacher = models.ForeignKey(
+        Teacher, on_delete=models.SET_NULL, null=True
+    )
 
     def __str__(self):
         return self.title
